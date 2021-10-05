8f120b067c18cc7f854018998ed8246529d85b03
mbeckdev <mbeckdev42@gmail.com>
2021-10-05 17:11:26 -0600

    Add initial files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import uniqid from 'uniqid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      listA: [0, 1, 2, 3, 4],
+    };
+  }
+
+  addToList = (e) => {
+    e.preventDefault();
+    console.log('adding now');
+    let numToAdd = '';
+    if (this.state.listA.length === 0) {
+      numToAdd = 0;
+    } else {
+      numToAdd = this.state.listA[this.state.listA.length - 1] + 1;
+    }
+
+    this.setState((prevState) => ({ listA: [...prevState.listA, numToAdd] }));
+  };
+
+  deleteLastFromList = (e) => {
+    e.preventDefault();
+    console.log('deleting now');
+    const newItemsInArray = this.state.listA.slice(
+      0,
+      this.state.listA.length - 1
+    );
+    this.setState((prevState) => ({ listA: [...newItemsInArray] }));
+  };
+
+  render() {
+    const listOfThings = this.state.listA.map((item) => (
+      <div key={uniqid()}>{item}</div>
+    ));
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>MBeckDev's Smallest React Project</h1>
+          <div>Add and Delete from this.state</div>
+          <div>no props or components</div>
+        </header>
+        <main>
+          <div className="main-sections-container">
+            <div className="add-section" onClick={this.addToList}>
+              <button>Add to list</button>
+            </div>
+
+            <div className="delete-section" onClick={this.deleteLastFromList}>
+              <button>Delete from list</button>
+            </div>
+
+            <div className="show-section">
+              <div>Current state shown here</div>
+              <div>The list is here:</div>
+
+              {listOfThings}
+            </div>
+          </div>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
